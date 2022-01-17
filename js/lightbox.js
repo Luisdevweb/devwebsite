ff66a11d578b96038c1ed123b076922a0e60b091
Luismac <luismachaca@outlook.com.pe>
2022-01-17 13:13:58 -0500

    HTML & css adaptable to mobile

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,25 +1,25 @@
-const Images=document.querySelectorAll(".gallery-img")
-const ImgLightBox=document.querySelector(".window-open")
-const ContainerImage=document.querySelector(".image-lightbox")
-const ImageClose=document.querySelector(".window-close");
-const BntMenu=document.querySelector(".menu")
+const Images = document.querySelectorAll(".gallery__img");
+const ImgLightBox = document.querySelector(".window-open");
+const ContainerImage = document.querySelector(".image-lightbox");
+const ImageClose = document.querySelector(".window-close");
+const BntMenu = document.querySelector(".menu");
 
-Images.forEach(img=>{
-    img.addEventListener("click",()=>{
-        let srcimg= img.getAttribute("src")
-        ContainerImage.classList.add("image-lightbox--show")
-        ImgLightBox.setAttribute("src",srcimg)
-        ImgLightBox.classList.add("show-image")
-        BntMenu.style.opacity="0"
-    })
-})
+Images.forEach((img) => {
+  img.addEventListener("click", () => {
+    let srcimg = img.getAttribute("src");
+    ContainerImage.classList.add("image-lightbox--show");
+    ImgLightBox.setAttribute("src", srcimg);
+    ImgLightBox.classList.add("show-image");
+    BntMenu.style.opacity = "0";
+  });
+});
 
-ContainerImage.addEventListener("click",e=>{
-    //console.log(e.target)
-    if(e.target==ImageClose){
-        ContainerImage.classList.remove("image-lightbox--show")
-        ImgLightBox.removeAttribute("src")
-        ImgLightBox.classList.remove("show-image")
-        BntMenu.style.opacity="1"
-    }
-})
+ContainerImage.addEventListener("click", (e) => {
+  //console.log(e.target)
+  if (e.target == ImageClose) {
+    ContainerImage.classList.remove("image-lightbox--show");
+    ImgLightBox.removeAttribute("src");
+    ImgLightBox.classList.remove("show-image");
+    BntMenu.style.opacity = "1";
+  }
+});
