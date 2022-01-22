8a27c6d4a94e4a094ab0c421c58ffe1b900eba76
Luismac <luismachaca@outlook.com.pe>
2022-01-21 23:48:58 -0500

    page responsive

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,8 @@
-const BtnMenu=document.querySelector(".menu")
-const NavBar=document.querySelector(".navbar")
+const btnMenu = document.querySelector(".menu");
+const navBar = document.querySelector(".navbar");
 
-BtnMenu.addEventListener("click",(e)=>{
-   NavBar.classList.add("navbar--translate")
-    //console.log(e.target)
- })
-
-window.addEventListener("click",(e)=>{
-    if(NavBar.classList.contains("navbar--translate") && e.target != NavBar && e.target!= BtnMenu){
-        NavBar.classList.remove("navbar--translate")
-    }
-})
+window.addEventListener("click", (e) => {
+  e.target.classList.contains("menu")
+    ? navBar.classList.add("navbar--translate")
+    : navBar.classList.remove("navbar--translate");
+});
